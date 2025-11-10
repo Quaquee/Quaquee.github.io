21313998154568b31b7816d1188084de42badd83
Quaquee <ryoung@addu.edu.ph>
2025-11-10 17:28:23 +0800

    Some CSS, Warnings for large inputs in sequence

--- Factorial/sequence.js
+++ Factorial/sequence.js
@@ -1,6 +1,7 @@
 document.getElementById("series").addEventListener("change",()=> 
 {
   document.getElementById("output").value = null;
+  document.getElementById("warning").innerHTML = null;
 });
 
 
@@ -16,37 +17,63 @@ document.getElementById("calculate").addEventListener("click", ()=>
   let select = document.getElementById("series").value;
   let N = document.getElementById("inputN").value * 1;
   let result = 0;
-    
+  document.getElementById("warning").innerHTML = null;
+
   //In Case of Invalid Input
-  if (N % 1 != 0 || N < 0 || N=="") {
+  if (N % 1 != 0 || N < 0 || N == "") {
     document.getElementById("output").value = null;
   }
 
   //N-Factorial Program
   else if (select == 0) {
     result = 1;
-    while (N > 1) {
-      result *= N;
-      N--;
+    if (N < 171) {
+      while (N > 1) {
+        result *= N;
+        N--;
+      }
+      document.getElementById("output").value = result;
     }
+
+    else {
+      document.getElementById("warning").innerHTML = "<strong>WARNING: The input is too large. MAX INPUT: 170<strong>"
+      document.getElementById("output").value = null;
+    }
+
   }
 
   //First N Numbers Sum Program
   else if (select == 1) {
     result = 0;
-    do {
-      result += N;
-      N--;
-    } while (N > 0);
+
+    if (N < 1e8) {
+      do {
+        result += N;
+        N--;
+      } while (N > 0);
+      document.getElementById("output").value = result;
+    }
+
+    else {
+      document.getElementById("warning").innerHTML = "<strong>WARNING: Large inputs may lead to crashing and loss of precision. MAX INPUT: 99 999 999<strong>"
+      document.getElementById("output").value = null;
+    }
   }
 
   //Average of first N Numbers Program
   else if (select == 2) {
-    result = 0;
-    for (let i = 0; i <= N; i++) {
-      result += i;
+    if (N < 1e8) {
+      result = 0;
+      for (let i = 0; i <= N; i++) {
+        result += i;
+      }
+      result /= N;
+      document.getElementById("output").value = result;
     }
-    result /= N;
+    
+    else {
+      document.getElementById("warning").innerHTML = "<strong>WARNING: Large inputs may lead to crashing and loss of precision. MAX INPUT: 99 999 999<strong>"
+      document.getElementById("output").value = null;
+    }
   }
-  document.getElementById("output").value = result;
 });
